ad4b1bb208385501e0b7c0f5c9dec513b9809f59
tracklist19 <162835686+tracklist19@users.noreply.github.com>
2024-04-08 22:25:29 +0200

    Add files via upload

---
+++ GeoWerkzeugPlugin/__init__.py
@@ -0,0 +1,5 @@
+from GeoWerkzeugPlugin.werkzeug import *
+
+
+def classFactory(iface):				 # Methode der init stellt iface (von QGIS) als Attribut der Klasse GeoWerkzeug in werkzeug.py bereit 
+    return GeoWerkzeug(iface)            # Instanz der Klasse wird erstellt, return gibt sie zurueck an QGIS -> QGIS platziert dann Plugin
